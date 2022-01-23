d761aec611b964e78d011911ce194ac07c358bc9
Rajeshri4321 <98088953+Rajeshri4321@users.noreply.github.com>
2022-01-23 19:50:40 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // int count = 8;
+            //count++;
+            //bool status = false;
+            //status = true;
+            //char ch = 'R';
+            String firstname = "ravi";
+            String lastname = " tambde";
+            String fullname = firstname + " " + lastname;
+            Console.WriteLine("Welcome to world of .net");
+            Console.WriteLine("Firstname " + firstname);
+            Console.ReadLine();
+        }
+    }
+}
